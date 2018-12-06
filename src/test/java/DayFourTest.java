29b0d702313341fc561d094643ed55575d36b48a
Psycho Ben <psychoben@ACAC5073.ipt.aol.com>
2018-12-06 09:43:32 +0000

    adding day four part one with tests

--- src/test/java/DayFourTest.java
+++ src/test/java/DayFourTest.java
@@ -1,34 +1,52 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DayFourTest {
+    private DayFour dayFour;
+
+    @Before
+    public void setup() {
+      this.dayFour = new DayFour();
+        String input =  "[1518-11-01 00:00] Guard #10 begins shift\n" +
+                "[1518-11-01 00:05] falls asleep\n" +
+                "[1518-11-01 00:25] wakes up\n" +
+                "[1518-11-01 00:30] falls asleep\n" +
+                "[1518-11-01 00:55] wakes up\n" +
+                "[1518-11-01 23:58] Guard #99 begins shift\n" +
+                "[1518-11-02 00:40] falls asleep\n" +
+                "[1518-11-02 00:50] wakes up\n" +
+                "[1518-11-03 00:05] Guard #10 begins shift\n" +
+                "[1518-11-03 00:24] falls asleep\n" +
+                "[1518-11-03 00:29] wakes up\n" +
+                "[1518-11-04 00:02] Guard #99 begins shift\n" +
+                "[1518-11-04 00:36] falls asleep\n" +
+                "[1518-11-04 00:46] wakes up\n" +
+                "[1518-11-05 00:03] Guard #99 begins shift\n" +
+                "[1518-11-05 00:45] falls asleep\n" +
+                "[1518-11-05 00:55] wakes up";
+        dayFour.setUp(input);
+    }
+
 
     @Test
     public void givenInput_gaurdTenSpendsFiftyMinutesAsleep() {
-        DayFour dayFour = new DayFour();
-        String input =  "[1518-11-01 00:00] Guard #10 begins shift\n" +
-                        "[1518-11-01 00:05] falls asleep\n" +
-                        "[1518-11-01 00:25] wakes up\n" +
-                        "[1518-11-01 00:30] falls asleep\n" +
-                        "[1518-11-01 00:55] wakes up\n" +
-                        "[1518-11-01 23:58] Guard #99 begins shift\n" +
-                        "[1518-11-02 00:40] falls asleep\n" +
-                        "[1518-11-02 00:50] wakes up\n" +
-                        "[1518-11-03 00:05] Guard #10 begins shift\n" +
-                        "[1518-11-03 00:24] falls asleep\n" +
-                        "[1518-11-03 00:29] wakes up\n" +
-                        "[1518-11-04 00:02] Guard #99 begins shift\n" +
-                        "[1518-11-04 00:36] falls asleep\n" +
-                        "[1518-11-04 00:46] wakes up\n" +
-                        "[1518-11-05 00:03] Guard #99 begins shift\n" +
-                        "[1518-11-05 00:45] falls asleep\n" +
-                        "[1518-11-05 00:55] wakes up";
-
-        dayFour.setUp(input);
-//        dayFour.calculateSleepTime();
-//        int result = dayFour.getGaurdSleepTime(1);
-//        assertEquals(50, result);
+        dayFour.calculateSleepTime();
+        int gaurdOneSleepTime = dayFour.getGaurdsSleepCount(10);
+        assertEquals(50, gaurdOneSleepTime);
     }
 
+    @Test
+    public void givenInput_gaurdNinetyNineSpendsThirtyMinutesAsleep() {
+        dayFour.calculateSleepTime();
+        int gaurdTwoSleepTime = dayFour.getGaurdsSleepCount(99);
+        assertEquals(30, gaurdTwoSleepTime);
+    }
+
+    @Test
+    public void givenInput_gaurdNinetyNineSleep240Minutes() {
+        int result = dayFour.calculateSleepTime();
+        assertEquals(240, result);
+    }
 }
